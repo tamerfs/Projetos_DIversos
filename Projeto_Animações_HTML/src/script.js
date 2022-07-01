fc260d3a9737da926f0e0036e8cf04e752a55047
tamerfs <tamer.srhn@gmail.com>
2022-07-01 18:01:18 -0300

    corrigindo

--- Projeto_Animações_HTML/src/script.js
+++ Projeto_Animações_HTML/src/script.js
@@ -1,13 +1,13 @@
 
 
 function textComand(){
-    document.getElementById("conteudoInterativo").innerHTML = "⁠Não há pactos entre homens e leões!"
+    document.getElementById("conteudoInterativo").innerHTML = "⁠'Não há pactos entre homens e leões!' - Aquiles"
  };
  
 
 
 function mouseOver(){
-    document.getElementById("mouseAlt").innerHTML = "⁠Se um dia contarem minha história, digam que andei com gigantes."
+    document.getElementById("mouseAlt").innerHTML = "⁠'Se um dia contarem minha história, digam que andei com gigantes.Homens se erguem,e caem,como trigos no inverno. Mas,jamais perecerão. Que digam que vivi na época de Heitor,domador de cavalos. Que digam que vivi na época de Aquiles.'   - Ulysses"
 }
 
 function mouseout(){
@@ -16,10 +16,18 @@ function mouseout(){
 }
 
 function mouseOver2(){
-    document.getElementById("mouseAlt2").innerHTML = "⁠Você me deu paz em uma vida de guerra."
+    document.getElementById("mouseAlt2").innerHTML = "⁠<img src='https://br.web.img2.acsta.net/medias/nmedia/18/90/43/26/20096298.jpg'/>"
 }
 
 function mouseout2(){
     document.getElementById("mouseAlt2").innerHTML = ""
+
+}
+function mouseOver3(){
+    document.getElementById("mouseAlt3").innerHTML = "⁠'Você me deu paz em uma vida de guerra.' - Aquiles"
+}
+
+function mouseout3(){
+    document.getElementById("mouseAlt3").innerHTML = ""
 
 }
